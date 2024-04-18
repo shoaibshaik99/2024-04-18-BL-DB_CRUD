cefdbc8e51c2e8dfe6d6a091a749ea08f4c081cc
shaikshoaib99 <smdshoaib137@gmail.com>
2024-04-18 19:34:58 +0530

    [Shoaib] Basic CRUD operation on MS SQL DB Using ADO.NET

---
+++ DB_CRUD/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace DB_CRUD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=Laptop\\SQLEXPRESS;Initial Catalog=LocalDB;Integrated Security=True";
+            SqlConnection sqlDBConnection = new SqlConnection(connectionString);
+            sqlDBConnection.Open();
+
+            bool loopIsActive = true;
+
+            while (loopIsActive)
+            {
+                Console.WriteLine("\nSelect an operation to perform on Details table in LocalDB Database" +
+                "\n1. Insert Data Into Table" +
+                "\n2. View Data Table" +
+                "\n3. Alter data into table" +
+                "\n4. Delete record from table" +
+                "\n5. Exit Program");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        //Insert Op
+                        SQLQuery.InsertQuery(sqlDBConnection);
+                        break;
+                    case 2:
+                        //Retrieve Op
+                        SQLQuery.RetrieveQuery(sqlDBConnection);
+                        break;
+                    case 3:
+                        //Update Op
+                        SQLQuery.RetrieveQuery(sqlDBConnection);
+                        SQLQuery.UpdateQuery(sqlDBConnection);
+                        SQLQuery.RetrieveQuery(sqlDBConnection);
+                        break;
+                    case 4:
+                        //Delete Op
+                        SQLQuery.RetrieveQuery(sqlDBConnection);
+                        SQLQuery.DeleteQuery(sqlDBConnection);
+                        SQLQuery.RetrieveQuery(sqlDBConnection);
+                        break;
+                    case 5:
+                        loopIsActive = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+
+                }
+            }
+            
+            sqlDBConnection.Close();
+        }
+    }
+}
---
+++ DB_CRUD/SQLQuery.cs
@@ -0,0 +1,67 @@
+﻿using System.Data.SqlClient;
+
+namespace DB_CRUD
+{
+    public static class SQLQuery
+    {
+        public static void InsertQuery(SqlConnection sqlDBConnection)
+        {
+            Console.WriteLine("Enter a username to add");
+            string usrName = Console.ReadLine();
+
+            Console.WriteLine("Enter the age of user");
+            int usrAge = Convert.ToInt32(Console.ReadLine());
+
+            string insertQuery = $"INSERT INTO Details(user_name,user_age) VALUES('{usrName}',{usrAge})";
+            SqlCommand insertCommand = new SqlCommand(insertQuery, sqlDBConnection);
+            insertCommand.ExecuteNonQuery();
+        }
+
+        public static void RetrieveQuery(SqlConnection sqlDBConnection)
+        {
+            string retrieveQuery = "SELECT * FROM Details";
+            SqlCommand retrieveCommand = new SqlCommand(retrieveQuery, sqlDBConnection);
+
+            SqlDataReader sqlDataReader = retrieveCommand.ExecuteReader();
+
+            Console.WriteLine("\n*****Start of Table****\n");
+
+            while(sqlDataReader.Read())
+            {
+                Console.WriteLine();
+                Console.Write($"User ID: {sqlDataReader.GetValue(0)}\t");
+                Console.Write($"User Name: {sqlDataReader.GetValue(1)}\t");
+                Console.WriteLine($"User Age: {sqlDataReader.GetValue(2)}");
+                Console.WriteLine();
+            }
+            sqlDataReader.Close();
+            Console.WriteLine("\n*****End of Table****\n");
+        }
+
+        public static void UpdateQuery(SqlConnection sqlDBConnection)
+        {
+            Console.WriteLine("Enter the user ID to update it's details");
+            int usrId = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the updated name");
+            string updName = Console.ReadLine();
+
+            Console.WriteLine("Enter the updated Age");
+            int updAge = Convert.ToInt32(Console.ReadLine());
+
+            string updateQuery = $"UPDATE Details SET user_name = '{updName}', user_age = {updAge} WHERE user_id = {usrId}";
+            SqlCommand updateCommand = new SqlCommand(updateQuery, sqlDBConnection);
+            updateCommand.ExecuteNonQuery();
+        }
+
+        public static void DeleteQuery(SqlConnection sqlDBConnection)
+        {
+            Console.WriteLine("Enter the user Id to delete");
+            int delUsrId = Convert.ToInt32(Console.ReadLine());
+
+            string deleteQuery = $"DELETE FROM Details\r\nWHERE user_id = {delUsrId}";
+            SqlCommand deleteCommand = new SqlCommand(deleteQuery, sqlDBConnection);
+            deleteCommand.ExecuteNonQuery();
+        }
+    }
+}
